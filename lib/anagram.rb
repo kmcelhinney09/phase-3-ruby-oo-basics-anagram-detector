e139d0ba0f0edfd7c0f0237dc5445665dfac630f
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-01-16 22:03:31 -0800

    commit tree 3b30be780db028291b5704b7138b99aae0012de8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+    def initialize (word)
+        @word = word
+    end
+    #needs to check if Anagram word matches to an anagram given to match
+    #must detect a match, simple letter match (not necessarily a word), more then one anagram
+    
+    def match (word_array)
+        #to find an anagram the letters in one word needs to match all the letters in another just in different order
+        #get letters of anagram word
+        letters_in_anagram_word = @word.chars.sort
+        #iterate through array of words get their letter and compare to anagram word
+        matching = []
+        word_array.each do |word_to_compare|
+            letters_to_compare = word_to_compare.chars.sort
+            if letters_to_compare == letters_in_anagram_word
+                matching << word_to_compare
+            end
+        end
+        matching        
+    end
+
+end
